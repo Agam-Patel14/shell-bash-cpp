035a893a9d154ef6ccd73657d58b598b31725d92
Agam Patel <patelagam560@gmail.com>
2026-06-30 12:15:48 +0530

    implement 'type' command functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -11,6 +12,7 @@ int main() {
     std::cout<<"$ ";
     std::string command;
     std::getline(std::cin,command);
+    std::vector<std::string> arr = {"echo" , "type" , "exit"};
 
     // commands
     if(command == "exit"){
@@ -18,6 +20,19 @@ int main() {
     }
     else if(command.substr(0,4) == "echo"){
       std::cout<<command.substr(5)<<std::endl;
+    }
+    else if(command.substr(0,4) == "type"){
+      bool found=false;
+      for(auto x : arr){
+        if(command.substr(5) == x){
+          found=true;
+          std::cout<<x<<" is a shell builtin"<<std::endl;
+          break;
+        }
+      }
+      if(!found){
+        std::cout<<command.substr(5)<<": not found"<<std::endl;
+      }
     }
     else{
       std::cout<<command<<": not found"<<std::endl;
