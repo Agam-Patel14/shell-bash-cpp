b7fec9a97ead99512e368ffff2a10efa653fbb16
Agam Patel <patelagam560@gmail.com>
2026-06-30 12:31:35 +0530

    refactor command handling in REPL for clarity and efficiency

--- src/main.cpp
+++ src/main.cpp
@@ -6,36 +6,54 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf; 
+  std::vector<std::string> arr = {"echo" , "type" , "exit"};
 
   // REPL
   while(1){
     std::cout<<"$ ";
-    std::string command;
-    std::getline(std::cin,command);
-    std::vector<std::string> arr = {"echo" , "type" , "exit"};
+    std::string line;
+    std::getline(std::cin,line);
+    std::string command,args="";
+    int ix=0,n=line.size();
+
+    // if the line is empty
+    if(line.empty()){
+      continue;
+    }
+
+    // reading the command and the args
+    while(ix<n){
+      if(line[ix]==' '){
+        break;
+      }
+      ix++;
+    }
+
+    command=line.substr(0,ix);
+    if(ix!=n) args=line.substr(ix+1);
 
     // commands
     if(command == "exit"){
       break;
     }
-    else if(command.substr(0,4) == "echo"){
-      std::cout<<command.substr(5)<<std::endl;
+    else if(command == "echo"){
+      std::cout<<args<<std::endl;
     }
-    else if(command.substr(0,4) == "type"){
+    else if(command == "type"){
       bool found=false;
-      for(auto x : arr){
-        if(command.substr(5) == x){
+      for(auto &x : arr){
+        if(args == x){
           found=true;
           std::cout<<x<<" is a shell builtin"<<std::endl;
           break;
         }
       }
       if(!found){
-        std::cout<<command.substr(5)<<": not found"<<std::endl;
+        std::cout<<args<<": not found"<<std::endl;
       }
     }
     else{
-      std::cout<<command<<": not found"<<std::endl;
+      std::cout<<line<<": not found"<<std::endl;
     }
   }
 }
